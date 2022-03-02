8244bcc390823b5ad6f327b48e4d4d1fa8df8102
DnC275 <275.denis@gmail.com>
2022-03-02 23:55:38 +0300

    room_code -> room_id

--- core/routing.py
+++ core/routing.py
@@ -4,5 +4,5 @@ from .consumers import GameConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r'^ws/game/(?P<room_code>\w+)/$', GameConsumer.as_asgi()),
+    re_path(r'^ws/game/(?P<room_id>\w+)/$', GameConsumer.as_asgi()),
 ]
