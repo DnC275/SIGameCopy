2b1eb87ee305d63a58503b49f6b85510d01a637e
DnC275 <275.denis@gmail.com>
2022-04-17 00:57:50 +0300

    fix cors

--- Svoyak_backend/settings.py
+++ Svoyak_backend/settings.py
@@ -178,6 +178,9 @@ CHANNEL_LAYERS = {
 if DEBUG:
     CORS_ALLOW_CREDENTIALS = True
     CORS_ORIGIN_ALLOW_ALL = True
+    CORS_ALLOW_HEADERS = (
+        'Authorization'
+    )
     # CORS_ORIGIN_WHITELIST = (
     #     'http://localhost:3000',
     #     'http://127.0.0.1:3000',
