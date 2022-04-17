a5774f3ede972e3b9291b92bd443cf55b5a11889
DnC275 <275.denis@gmail.com>
2022-04-17 23:58:24 +0300

    fix cors

--- Svoyak_backend/settings.py
+++ Svoyak_backend/settings.py
@@ -193,19 +193,19 @@ CORS_ORIGIN_ALLOW_ALL = True
     # CORS_ALLOW_CREDENTIALS = True
     # CORS_ALLOW_ALL_ORIGINS = True
     # CORS_ORIGIN_ALLOW_ALL = True
-    # CORS_ALLOW_HEADERS = [
-    #     'accept',
-    #     'accept-encoding',
-    #     'authorization',
-    #     'content-type',
-    #     'dnt',
-    #     'origin',
-    #     'user-agent',
-    #     'x-csrftoken',
-    #     'x-requested-with',
-    #     'Access-Control-Allow-Headers',
-    #     'Access-Control-Allow-Credentials',
-    # ]
+    CORS_ALLOW_HEADERS = [
+        'accept',
+        'accept-encoding',
+        'authorization',
+        'content-type',
+        'dnt',
+        'origin',
+        'user-agent',
+        'x-csrftoken',
+        'x-requested-with',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Credentials',
+    ]
     # CORS_ALLOW_METHODS = [
     #     "DELETE",
     #     "GET",
